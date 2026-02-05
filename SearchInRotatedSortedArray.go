17e712ad50ee510a7fdb2537b9165087db259226
vrinda1996 <vrinda5252@gmail.com>
2026-02-05 15:35:14 +0530

    Create SearchInRotatedSortedArray.go

---
+++ SearchInRotatedSortedArray.go
@@ -0,0 +1,22 @@
+func search(nums []int, target int) int {
+    low, high := 0, len(nums)-1
+    for low <= high {
+        mid:= low + (high - low)/2
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] >= nums[low] {
+            if nums[low] <= target && target < nums[mid]{
+                high = mid - 1
+            } else {
+                low = mid+1
+            }    
+        } else {
+            if nums[mid] <= target && target <= nums[high]{
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+    }
+    return -1 
+}
